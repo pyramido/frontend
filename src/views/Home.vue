fb35f49075c4771fdfe34d3c3fbb04940eb45ea2
Keven Lefebvre <orditeck@gmail.com>
2018-09-29 19:38:03 -0400

    working on stylings

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,46 +1,100 @@
 <template>
   <div id="home">
-    <Carousel/>
-    <hr >
-    <div class="card" align="center">
-      <div class="card-body">
-        <h5 class="card-title">Special title treatment</h5>
-        <p class="card-text">
-          With supporting text below as a natural lead-in to additional content.
-        </p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
+    <div class="p-slider">
+      <div class="p-slide"></div>
+    </div>
+    <div class="p-cards">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6">
+            <div class="p-card">
+              <div class="row">
+                <div class="col-md-4">
+                  <img src="/img/icons-07.png"/>
+                </div>
+                <div class="col-md-8">
+                  <h4>Acteurs culturels</h4>
+                  <p>
+                    Petit texte de description des acteurs culturels : possibilité du site
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="p-card">
+              <div class="row">
+                <div class="col-md-4">
+                  <img src="/img/icons-08.png"/>
+                </div>
+                <div class="col-md-8">
+                  <h4>Entreprises locales</h4>
+                  <p>
+                    Petit texte de description des entreprises : possibilité du site
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="p-card">
+              <div class="row">
+                <div class="col-md-4">
+                  <img src="/img/icons-09.png"/>
+                </div>
+                <div class="col-md-8">
+                  <h4>Particuliers</h4>
+                  <p>
+                    Petit texte de description des particuliers culturels : possibilité du site
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6 right">
+            <h2>Culture & cie connecte les événements culturels et les entreprises</h2>
+            <p>
+              Sed erat aequius Triarium aliquid de dissensione nostra iudicare. Praeteritis, inquit, gaudeo.
+              Quae fere omnia appellantur uno ingenii nomine, easque virtutes qui habent, ingeniosi vocantur.
+            </p>
+          </div>
+        </div>
       </div>
     </div>
-    <hr>
-    <b-container fluid>
-      <b-row>
-        <b-col align="center">
-          <UseCaseCard
-            title="Acteurs culturels"
-            description="Petit texte de description des acteurs culturels : possibilité du site"
-            img="img/theatre-masks.png"
-            button-text="Créer un événément"/>
-        </b-col>
-        <b-col align="center">
-          <UseCaseCard
-            title="Entreprises locales"
-            description="Petit texte de description des entreprises : possibilité du site"
-            img="img/theatre-masks.png"
-            button-text="Parrainer un événément"/>
-        </b-col>
-        <b-col align="center">
-          <UseCaseCard
-            title="Particuliers"
-            description="Petit texte de description
-             des particuliers culturels : possibilité du site"
-            img="img/theatre-masks.png"
-            button-text="Participer à un événement"/>
-        </b-col>
+    <div class="p-featured">
+      <div class="container">
+        <h2>Événements en vedette</h2>
 
-      </b-row>
-    </b-container>
-    <hr >
-
+        <div class="p-items-container">
+          <div class="p-item">
+            <div class="p-thumbnail">
+              <img src="https://placeimg.com/320/260/any?1"/>
+              <div class="p-date">8 sept. 2018</div>
+            </div>
+            <h4>Un super titre</h4>
+            <p>
+              Non quam nostram quidem, inquit Pomponius iocans; Hoc dixerit potius Ennius...
+            </p>
+          </div>
+          <div class="p-item">
+            <div class="p-thumbnail">
+              <img src="https://placeimg.com/320/260/any?2"/>
+              <div class="p-date">8 sept. 2018</div>
+            </div>
+            <h4>Un super titre</h4>
+            <p>
+              Non quam nostram quidem, inquit Pomponius iocans; Hoc dixerit potius Ennius...
+            </p>
+          </div>
+          <div class="p-item">
+            <div class="p-thumbnail">
+              <img src="https://placeimg.com/320/260/any?3"/>
+              <div class="p-date">8 sept. 2018</div>
+            </div>
+            <h4>Un super titre</h4>
+            <p>
+              Non quam nostram quidem, inquit Pomponius iocans; Hoc dixerit potius Ennius...
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
