6a5be1c439a3086926dd5063768ec453ca0247d6
Emile Bergeron <emileber@users.noreply.github.com>
2018-09-29 11:26:47 -0400

    Routes and pages (#3)

--- src/router.js
+++ src/router.js
@@ -1,6 +1,9 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home';
+import About from './views/About';
+import EventListing from './views/events/EventListing';
+import EventPage from './views/events/EventPage';
 
 Vue.use(Router);
 
@@ -10,6 +13,23 @@ export default new Router({
             path: '/',
             name: 'home',
             component: Home,
+        },
+        {
+            path: '/about',
+            name: 'about',
+            component: About,
+        },
+        {
+            path: '/events',
+            name: 'events',
+            component: EventListing,
+            children: [
+                {
+                    path: ':id',
+                    name: 'event-page',
+                    component: EventPage,
+                },
+            ],
         },
     ],
 });
