b33a77a6b026ec6881b819d11f66c09630da75a8
uazech <43653336+uazech@users.noreply.github.com>
2018-09-29 09:45:05 -0400

    Ajout du layout global (#1)
    Ajout du footer et de la navbar

---
+++ src/components/Footer.vue
@@ -0,0 +1,17 @@
+<template>
+  <footer  class="footer">
+      <b-container class="bv-example-row">
+        <b-row>
+            <b-col align="center">Footer 1</b-col>
+            <b-col align="center">Footer 2</b-col>
+            <b-col align="center">Footer 3</b-col>
+        </b-row>
+      </b-container>
+    </footer>
+</template>
+<script>
+export default {
+  name: 'Footer',
+  
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -2,9 +2,23 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import BootstrapVue from 'bootstrap-vue'
+import bModal from 'bootstrap-vue/es/components/modal/modal'
+import bModalDirective from 'bootstrap-vue/es/directives/modal/modal'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 import './registerServiceWorker';
 
+
 Vue.config.productionTip = false;
+Vue.use(BootstrapVue);
+
+// Components
+Vue.component('b-modal', bModal);
+Vue.directive('b-modal', bModalDirective);
+
 
 new Vue({
   router,
