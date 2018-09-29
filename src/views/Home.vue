31ff2680c8752d0d79e7bed0fe5d1623705114a8
Unknown <baptiste.chezaubernard@gmail.com>
2018-09-29 10:09:24 -0400

    Ajout du carousel

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,20 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+  <div id="home">
+      <Carousel/>
+      <b-container fluid>
+        <b-row/>
+      </b-container>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
+import Carousel from '@/components/Carousel.vue'; 
 
 export default {
   name: 'home',
+  components: {
+    Carousel,
+  },
 
 };
 </script>
