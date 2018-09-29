93c0607e92ec81443eda5f5088974fd13dde639b
Emile Bergeron <emileber@users.noreply.github.com>
2018-09-29 10:49:58 -0400

    Feature/config (#2)
    * Adds linting
    
    * Fixes linting

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,27 @@
 <template>
-  <div id="app">
-    <Navbar/>
+    <div id="app">
+        <Navbar/>
 
-    <router-view/>
-    <Footer/>
-  </div>
-  
+        <router-view/>
+        <Footer/>
+    </div>
 
 </template>
 
-
 <script>
 // @ is an alias to /src
-import Footer from '@/components/Footer.vue'; 
-import Navbar from '@/components/Navbar.vue'; 
-
+import Footer from '@/components/Footer';
+import Navbar from '@/components/Navbar';
 
 export default {
-  name: 'app',
-  components: {
-    Footer,
-    Navbar,
-  },
+    name: 'App',
+    components: {
+        Footer,
+        Navbar,
+    },
 };
 </script>
 <style lang="scss" scoped>
 @import 'style/app.scss'
 
-
 </style>
-
--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,17 +1,17 @@
 <template>
-  <footer  class="footer">
-      <b-container class="bv-example-row">
-        <b-row>
-            <b-col align="center">Footer 1</b-col>
-            <b-col align="center">Footer 2</b-col>
-            <b-col align="center">Footer 3</b-col>
-        </b-row>
-      </b-container>
+    <footer class="footer">
+        <b-container class="bv-example-row">
+            <b-row>
+                <b-col align="center">Footer 1</b-col>
+                <b-col align="center">Footer 2</b-col>
+                <b-col align="center">Footer 3</b-col>
+            </b-row>
+        </b-container>
     </footer>
 </template>
 <script>
 export default {
-  name: 'Footer',
-  
+    name: 'Footer',
+
 };
 </script>
