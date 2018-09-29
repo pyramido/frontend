296c55c2b950cfe25feaee6bc4ff80045b6704ee
Mathieu Auclair <mathiass12@hotmail.com>
2018-09-29 14:48:02 -0400

    Travis setup (#8)
    * add travis file & update e2e test
    
    * remove e2e execution
    
    * remove useless e2e tests
    
    * fix sample test

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,12 @@
-// import { shallowMount } from '@vue/test-utils';
-// import HelloWorld from '@/components/HelloWorld';
+import { shallowMount } from '@vue/test-utils';
+import HelloWorld from '@/components/Footer.vue';
 
-// describe('HelloWorld.vue', () => {
-//     it('renders props.msg when passed', () => {
-//         const msg = 'new message';
-//         const wrapper = shallowMount(HelloWorld, {
-//             propsData: { msg },
-//         });
-//         expect(wrapper.text()).toMatch(msg);
-//     });
-// });
+describe('Sample test', () => {
+  it('this is a test sample', () => {
+    const msg = 'new message';
+    const wrapper = shallowMount(HelloWorld, {
+      propsData: { msg },
+    });
+    expect("sample").toMatch("sample");
+  });
+});
