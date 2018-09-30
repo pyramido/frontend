f1071424433a13e6496a7dca88082e1476042e15
Keven Lefebvre <orditeck@gmail.com>
2018-09-29 21:03:48 -0400

    styling

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,10 +9,10 @@
           <div class="col-md-6">
             <div class="p-card">
               <div class="row">
-                <div class="col-md-4">
-                  <img src="/img/icons-07.png"/>
+                <div class="p-left col-md-3">
+                  <div><img src="/img/icons-10.png"/></div>
                 </div>
-                <div class="col-md-8">
+                <div class="p-right col-md-9">
                   <h4>Acteurs culturels</h4>
                   <p>
                     Petit texte de description des acteurs culturels : possibilité du site
@@ -22,10 +22,10 @@
             </div>
             <div class="p-card">
               <div class="row">
-                <div class="col-md-4">
-                  <img src="/img/icons-08.png"/>
+                <div class="p-left col-md-3">
+                  <div><img src="/img/icons-11.png"/></div>
                 </div>
-                <div class="col-md-8">
+                <div class="p-right col-md-9">
                   <h4>Entreprises locales</h4>
                   <p>
                     Petit texte de description des entreprises : possibilité du site
@@ -35,10 +35,10 @@
             </div>
             <div class="p-card">
               <div class="row">
-                <div class="col-md-4">
-                  <img src="/img/icons-09.png"/>
+                <div class="p-left col-md-3">
+                  <div><img src="/img/icons-12.png"/></div>
                 </div>
-                <div class="col-md-8">
+                <div class="p-right col-md-9">
                   <h4>Particuliers</h4>
                   <p>
                     Petit texte de description des particuliers culturels : possibilité du site
@@ -53,44 +53,62 @@
               Sed erat aequius Triarium aliquid de dissensione nostra iudicare. Praeteritis, inquit, gaudeo.
               Quae fere omnia appellantur uno ingenii nomine, easque virtutes qui habent, ingeniosi vocantur.
             </p>
+
+            <div class="mockup">
+              <img src="/img/mockup.jpg"/>
+            </div>
           </div>
         </div>
       </div>
     </div>
     <div class="p-featured">
       <div class="container">
-        <h2>Événements en vedette</h2>
+        <div class="row">
+          <div class="col-md-6">
+            <h2>Événements en vedette</h2>
 
-        <div class="p-items-container">
-          <div class="p-item">
-            <div class="p-thumbnail">
-              <img src="https://placeimg.com/320/260/any?1"/>
-              <div class="p-date">8 sept. 2018</div>
+            <div class="p-items-container">
+              <div class="p-item">
+                <div class="p-thumbnail">
+                  <img src="https://placeimg.com/320/260/any?1"/>
+                  <div class="p-date">8 sept. 2018</div>
+                </div>
+                <h4>Un super titre</h4>
+                <p>
+                  Non quam nostram quidem, inquit Pomponius iocans; Hoc dixerit potius Ennius...
+                </p>
+              </div>
+              <div class="p-item">
+                <div class="p-thumbnail">
+                  <img src="https://placeimg.com/320/260/any?2"/>
+                  <div class="p-date">8 sept. 2018</div>
+                </div>
+                <h4>Un super titre</h4>
+                <p>
+                  Non quam nostram quidem, inquit Pomponius iocans; Hoc dixerit potius Ennius...
+                </p>
+              </div>
+              <div class="p-item">
+                <div class="p-thumbnail">
+                  <img src="https://placeimg.com/320/260/any?3"/>
+                  <div class="p-date">8 sept. 2018</div>
+                </div>
+                <h4>Un super titre</h4>
+                <p>
+                  Non quam nostram quidem, inquit Pomponius iocans; Hoc dixerit potius Ennius...
+                </p>
+              </div>
+              <div class="p-item">
+                <div class="p-thumbnail">
+                  <img src="https://placeimg.com/320/260/any?4"/>
+                  <div class="p-date">8 sept. 2018</div>
+                </div>
+                <h4>Un super titre</h4>
+                <p>
+                  Non quam nostram quidem, inquit Pomponius iocans; Hoc dixerit potius Ennius...
+                </p>
+              </div>
             </div>
-            <h4>Un super titre</h4>
-            <p>
-              Non quam nostram quidem, inquit Pomponius iocans; Hoc dixerit potius Ennius...
-            </p>
-          </div>
-          <div class="p-item">
-            <div class="p-thumbnail">
-              <img src="https://placeimg.com/320/260/any?2"/>
-              <div class="p-date">8 sept. 2018</div>
-            </div>
-            <h4>Un super titre</h4>
-            <p>
-              Non quam nostram quidem, inquit Pomponius iocans; Hoc dixerit potius Ennius...
-            </p>
-          </div>
-          <div class="p-item">
-            <div class="p-thumbnail">
-              <img src="https://placeimg.com/320/260/any?3"/>
-              <div class="p-date">8 sept. 2018</div>
-            </div>
-            <h4>Un super titre</h4>
-            <p>
-              Non quam nostram quidem, inquit Pomponius iocans; Hoc dixerit potius Ennius...
-            </p>
           </div>
         </div>
       </div>
