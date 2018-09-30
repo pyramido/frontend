1cc7972439ebe3a9da469632d04360efddf7a366
Emile Bergeron <ber.emile@gmail.com>
2018-09-30 12:18:20 -0400

    deploy

---
+++ precache-manifest.60b1f46ef1d2e8fe18fc3d36879f5983.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = [
+  {
+    "revision": "2ae6a2dabcc14cc5e9ff3f51410948f2",
+    "url": "/img/icons-10.png"
+  },
+  {
+    "revision": "5027bc28ad6902a8198ead9cca1459d3",
+    "url": "/img/placeholders/i5.jpg"
+  },
+  {
+    "revision": "091ebad216de85eafb4e5c7209d46686",
+    "url": "/img/triangles.png"
+  },
+  {
+    "revision": "966f073d4674dab7db66",
+    "url": "/js/chunk-vendors.ca2b81a8.js"
+  },
+  {
+    "revision": "d12edca9a56cc5034abe09f4e8657a88",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d0234fbcd8b1fda1e0a6",
+    "url": "/js/app.24ef3a88.js"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "00b3161f7c703e45c80a44a88ee21d1d",
+    "url": "/img/theatre-masks.png"
+  },
+  {
+    "revision": "52efad075ca9b8a7d18fc051eb70286c",
+    "url": "/img/icons-07.png"
+  },
+  {
+    "revision": "bed37aad870b2ef5f78df5a2de18efa6",
+    "url": "/img/map.svg"
+  },
+  {
+    "revision": "e0fde58a85d20eea395a296b3f09d5b4",
+    "url": "/img/placeholders/i4.jpg"
+  },
+  {
+    "revision": "38511f1b03d1992c90fa1934c21d6ec3",
+    "url": "/img/placeholders/i3.jpg"
+  },
+  {
+    "revision": "cc53fd573d041f73eb86d238378228d1",
+    "url": "/img/placeholders/i2.jpg"
+  },
+  {
+    "revision": "2d1945b415f38bfb156d1a681658884a",
+    "url": "/img/mockup.jpg"
+  },
+  {
+    "revision": "0ee3c56e446dd666d0de2ee907729dfe",
+    "url": "/img/logo.png"
+  },
+  {
+    "revision": "df620070a34a9af9a51bf0f2ab5e0780",
+    "url": "/img/facebook-logo.png"
+  },
+  {
+    "revision": "43796618d370e9cfef65d9d20bc1a08d",
+    "url": "/img/icons-02.png"
+  },
+  {
+    "revision": "07cd61a8756ac19cc16a0d938e992b69",
+    "url": "/img/icons-03.png"
+  },
+  {
+    "revision": "2393e21b6915dc14bc1509ae7f27cad1",
+    "url": "/img/icons-11.png"
+  },
+  {
+    "revision": "147216e9d3c8c69e220718ee0b9ebf39",
+    "url": "/img/icons-08.png"
+  },
+  {
+    "revision": "5a01af174172981323ee265ba6062407",
+    "url": "/img/icons-09.png"
+  },
+  {
+    "revision": "238ea58a05deb7d70576516f52a31dfd",
+    "url": "/img/icons-01.png"
+  },
+  {
+    "revision": "0b59f7b83e15d58739d606147353b37a",
+    "url": "/img/icons-06.png"
+  },
+  {
+    "revision": "d1838cf7e4e17d3c9fec8f0830f3f4cb",
+    "url": "/img/icons-12.png"
+  },
+  {
+    "revision": "43a6591c6b46d2ce71df8a8093f8f468",
+    "url": "/img/icons-05.png"
+  },
+  {
+    "revision": "ab120b8f7c2521b1bc24ba0d11b683e9",
+    "url": "/img/icons-04.png"
+  },
+  {
+    "revision": "c711e8edbb0ad45b72601054b7eba120",
+    "url": "/img/event-1.jpg"
+  },
+  {
+    "revision": "966f073d4674dab7db66",
+    "url": "/css/chunk-vendors.61ee30a5.css"
+  },
+  {
+    "revision": "d0234fbcd8b1fda1e0a6",
+    "url": "/css/app.d40a61aa.css"
+  }
+];
